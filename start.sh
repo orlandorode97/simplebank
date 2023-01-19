b2b8b246c1eef185f3603a81af768ce1b0000f11
Orlando Romo <orlando.romo.97@hotmail.com>
2023-01-19 17:17:57 -0600

    another change

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 # Exit in case there is an error.
 set -e
 echo "run db migration"
+source app.env
 ./goose_linux_x86_64 -dir migrations/ -table schema_migrations postgres "$DB_SOURCE" up
 echo "start the app"
 ## Take all parameters to the script and run it
