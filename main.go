dc372044b6f4a6a4ef72290ace9ec84256df4d02
Orlando Romo <orlando.romo.97@hotmail.com>
2024-02-13 15:49:44 -0600

    first commit

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 type User struct {
 	FirstName      string     `validate:"required"`
 	LastName       string     `validate:"required"`
-	Age            uint8      `validate:"gte=0,lte=130"`
+	Age            uint8      `validate:"gte=0,lte=100"`
 	Email          string     `validate:"required,email"`
 	FavouriteColor string     `validate:"iscolor"`                // alias for 'hexcolor|rgb|rgba|hsl|hsla'
 	Addresses      []*Address `validate:"required,dive,required"` // a person can have a home and cottage...
